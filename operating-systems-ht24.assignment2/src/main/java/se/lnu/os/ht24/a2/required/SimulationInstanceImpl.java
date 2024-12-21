4c925839b8e96d19068c2173053301f2b288184a
Tiago Perrupato Antunes <tiagoperrupato@gmail.com>
2024-12-21 15:08:21 +0100

    feat: add basic implementation with allocation functions, without testing

--- operating-systems-ht24.assignment2/src/main/java/se/lnu/os/ht24/a2/required/SimulationInstanceImpl.java
+++ operating-systems-ht24.assignment2/src/main/java/se/lnu/os/ht24/a2/required/SimulationInstanceImpl.java
@@ -3,6 +3,9 @@ package se.lnu.os.ht24.a2.required;
 import se.lnu.os.ht24.a2.provided.data.StrategyType;
 import se.lnu.os.ht24.a2.provided.abstract_.Instruction;
 import se.lnu.os.ht24.a2.provided.exceptions.InstructionException;
+import se.lnu.os.ht24.a2.provided.instructions.AllocationInstruction;
+import se.lnu.os.ht24.a2.provided.instructions.CompactInstruction;
+import se.lnu.os.ht24.a2.provided.instructions.DeallocationInstruction;
 import se.lnu.os.ht24.a2.provided.interfaces.Memory;
 import se.lnu.os.ht24.a2.provided.interfaces.SimulationInstance;
 
@@ -32,6 +35,9 @@ public class SimulationInstanceImpl implements SimulationInstance {
              hole for an AllocationInstruction, you should choose the one with the lowest address among them.
              For FIRST_FIT, always start from the address 0 when searching for a valid hole.
          */
+        while (!remainingInstructions.isEmpty()) {
+            executeNextInstruction();
+        }
     }
 
     @Override
@@ -40,6 +46,9 @@ public class SimulationInstanceImpl implements SimulationInstance {
             Implement the method to run a stepped simulation (one step = one instruction). If steps > actual available
             instructions, just run all the simulation.
          */
+        for (int i = 0; i < steps && !remainingInstructions.isEmpty(); i++) {
+            executeNextInstruction();
+        }
     }
 
     @Override
@@ -69,5 +78,50 @@ public class SimulationInstanceImpl implements SimulationInstance {
                 "List of Remaining Instructions: " + remainingInstructions + "\n" +
                 "Current Memory Structure:\n\n" + memory + "\n" +
                 "List of Occurred Exceptions: " + instructionExceptions;
+    }
+
+    /**
+     * Helper method to execute the next instruction in the queue.
+     */
+    private void executeNextInstruction() {
+        Instruction instruction = remainingInstructions.poll();
+        if (instruction == null) return;
+
+        try {
+            if (instruction instanceof AllocationInstruction alloc) {
+                executeAllocation(alloc);
+            } else if (instruction instanceof DeallocationInstruction dealloc) {
+                executeDeallocation(dealloc);
+            } else if (instruction instanceof CompactInstruction) {
+                executeCompaction();
+            }
+        } catch (InstructionException ex) {
+            instructionExceptions.add(ex);
+        }
+    }
+
+    /**
+     * Execute an allocation instruction.
+     */
+    private void executeAllocation(AllocationInstruction alloc) throws InstructionException {
+        memory.allocate(alloc.getProcessId(), alloc.getSize(), strategyType);
+    }
+
+    /**
+     * Execute a deallocation instruction.
+     */
+    private void executeDeallocation(DeallocationInstruction dealloc) throws InstructionException {
+        boolean success = memory.deallocate(dealloc.getProcessId());
+
+        if (!success) {
+            throw new InstructionException("Deallocation failed", memory.largestFreeBlock());
+        }
+    }
+
+    /**
+     * Execute a compaction instruction.
+     */
+    private void executeCompaction() {
+        memory.compact();
     }
 }
